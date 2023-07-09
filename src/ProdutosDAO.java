b50d1928476cfd9539055d8e314f8c4aa4cf3754
Marceosantos22 <marceosantos@gmail.com>
2023-07-09 00:16:13 -0300

    função deve buscar todos os produtos no banco de dados com o status (Vendido)

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -58,7 +58,7 @@ public class ProdutosDAO {
 	public ArrayList<ProdutosDTO> listarProdutos() {
 
 		String sql = "SELECT * FROM produtos";
-		
+
 		try {
 
 			conexao.connectDB();
@@ -94,10 +94,9 @@ public class ProdutosDAO {
 
 	}
 
-	public int venderProduto( int id) {
+	public int venderProduto(int id) {
 
 		int status;
-		
 
 		try {
 
@@ -121,6 +120,45 @@ public class ProdutosDAO {
 
 			return ex.getErrorCode();
 
+		}
+
+	}
+
+	public List<ProdutosDTO> listaTableVendas(String statusVenda) {
+
+		String sql = "SELECT * FROM produtos WHERE status = ? ";
+
+		try {
+
+			conexao.connectDB();
+			Connection conn = conexao.getConexao();
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			st.setString(1, statusVenda);
+
+			ResultSet rs = st.executeQuery();
+
+			List<ProdutosDTO> lista = new ArrayList<>();
+
+			while (rs.next()) {
+
+				ProdutosDTO produto = new ProdutosDTO();
+
+				produto.setId(rs.getInt("id"));
+				produto.setNome(rs.getString("nome"));
+				produto.setValor(rs.getInt("valor"));
+				produto.setStatus(rs.getString("status"));
+
+				lista.add(produto);
+
+			}
+
+			return lista;
+
+		} catch (SQLException ex) {
+
+			System.out.println("Erro ao pesquisar: " + ex.getMessage());
+			return null;
 		}
 
 	}
